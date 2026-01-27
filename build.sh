2b6e85b10ea7268935694ce972543f28a03193b9
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-01-27 10:23:48 -0500

    Update script to build images

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 cd Dockerfiles
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 224382219437.dkr.ecr.us-east-1.amazonaws.com 
+docker buildx build --platform linux/amd64 --file Dockerfile.ollama --tag 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-ollama:$(git rev-parse HEAD) .
+docker buildx build --platform linux/amd64 --file Dockerfile.langflow --tag 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-langflow:$(git rev-parse HEAD) .
 
-docker build --file Dockerfile.langflow --tag 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-langflow:$(git rev-parse HEAD) --push .
-docker build --file Dockerfile.ollama --tag 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-ollama:$(git rev-parse HEAD) --push .
-docker build --file Dockerfile.opensearch --tag 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-opensearch:$(git rev-parse HEAD) --push .
+docker push 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-ollama:$(git rev-parse HEAD)
+docker push 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-langflow:$(git rev-parse HEAD)
