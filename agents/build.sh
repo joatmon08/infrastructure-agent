3494250c3ba1c9bb13a371a2d2ad52c1494f5f90
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-02-10 10:38:52 -0500

    Rebuild agent and test-client

--- agents/build.sh
+++ agents/build.sh
@@ -6,3 +6,8 @@ cd helloworld
 
 docker buildx build --platform linux/amd64 --file Dockerfile --tag 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-helloworld-agent:$(git rev-parse HEAD) .
 docker push 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-helloworld-agent:$(git rev-parse HEAD)
+
+cd ../test-client
+
+docker buildx build --platform linux/amd64 --file Dockerfile --tag 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-test-client:$(git rev-parse HEAD) .
+docker push 224382219437.dkr.ecr.us-east-1.amazonaws.com/infra-agent-test-client:$(git rev-parse HEAD)
