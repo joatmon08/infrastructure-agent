416b95664b668b66df20e393bc24eb2b96bd93d9
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-02-26 10:55:54 -0500

    Redeploy ingress after CRD

--- terraform/kubernetes/kubernetes.tf
+++ terraform/kubernetes/kubernetes.tf
@@ -36,6 +36,7 @@ resource "kubernetes_ingress_v1" "helloworld_agent_server" {
       }
     }
   }
+  depends_on = [kubernetes_ingress_class_v1.alb, kubernetes_manifest.ingressclassparams_alb]
 }
 
 resource "kubernetes_config_map_v1" "helloworld_agent_server" {
