f8df666b1d7924b60b0bccc855713f7931243e97
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-02-26 10:58:59 -0500

    Remove AWS provider from Kubernetes Terraform deployments

--- terraform/kubernetes/versions.tf
+++ terraform/kubernetes/versions.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 3.0"
