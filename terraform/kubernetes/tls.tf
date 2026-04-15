dc07d62e7141eb2ed84365aab50874159bc40c45
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-04-15 15:40:36 -0400

    Change requestor to EKS specific

--- terraform/kubernetes/tls.tf
+++ terraform/kubernetes/tls.tf
@@ -29,7 +29,7 @@ resource "kubernetes_certificate_signing_request_v1" "vault" {
   }
 
   spec {
-    signer_name = "kubernetes.io/kubelet-serving"
+    signer_name = "beta.eks.amazonaws.com/app-serving"
 
     expiration_seconds = 8640000
 
