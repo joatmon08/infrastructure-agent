c7cab7d341b07070f012d28cf19bf476c3244507
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-04-16 14:02:36 -0400

    Enable audit logs to stdout

--- scripts/vault-init.sh
+++ scripts/vault-init.sh
@@ -7,5 +7,7 @@ SHA256=$(kubectl exec -n vault vault-0 -- sha256sum /vault/plugins/vault-plugin-
 
 source secrets.env
 
+vault audit enable file file_path=stdout
+
 vault plugin register -sha256=$SHA256 secret vault-plugin-secrets-oauth-token-exchange
 vault plugin info secret vault-plugin-secrets-oauth-token-exchange
