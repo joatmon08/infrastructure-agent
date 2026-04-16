286666b2603d263462385e7e26764815bca9d720
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-04-16 13:10:24 -0400

    Write as text instead of JSON for Vault keys

--- scripts/vault-init.sh
+++ scripts/vault-init.sh
@@ -2,7 +2,6 @@
 
 mkdir -p secrets
 
-export VAULT_FORMAT=json
 KEYS=$(kubectl exec -it -n vault vault-0 -- vault operator init)
 echo $KEYS > secrets/vault-init.txt
 kubectl exec -it -n vault vault-0 -- vault status
