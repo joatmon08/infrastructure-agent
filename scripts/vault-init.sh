037459bb2cebef7b5567bc488c24171a33e7a370
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-04-16 13:57:01 -0400

    Register plugin as part of initialization

--- scripts/vault-init.sh
+++ scripts/vault-init.sh
@@ -2,3 +2,10 @@
 
 kubectl exec -n vault vault-0 -- vault operator init > secrets/vault-init.txt
 kubectl exec -n vault vault-0 -- vault status
+
+SHA256=$(kubectl exec -n vault vault-0 -- sha256sum /vault/plugins/vault-plugin-secrets-oauth-token-exchange | cut -d ' ' -f1)      
+
+source secrets.env
+
+vault plugin register -sha256=$SHA256 secret vault-plugin-secrets-oauth-token-exchange
+vault plugin info secret vault-plugin-secrets-oauth-token-exchange
