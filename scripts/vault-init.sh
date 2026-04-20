232e561df7d87c9d29ffed21d40295e4a1bf9174
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-04-20 13:00:03 -0400

    Pause to initialize Vault and get token before loading plugin

--- scripts/vault-init.sh
+++ scripts/vault-init.sh
@@ -3,6 +3,8 @@
 kubectl exec -n vault vault-0 -- vault operator init > secrets/vault-init.txt
 kubectl exec -n vault vault-0 -- vault status
 
+read "Copy the root token from secrets/vault-init.txt and put it in secrets.env as `export VAULT_TOKEN=<token>`"
+
 SHA256=$(kubectl exec -n vault vault-0 -- sha256sum /vault/plugins/vault-plugin-secrets-oauth-token-exchange | cut -d ' ' -f1)      
 
 source secrets.env
