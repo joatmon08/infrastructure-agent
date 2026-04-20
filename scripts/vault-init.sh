12b7b7d74f332a29c55876296f411f34b26fb01b
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-04-20 15:19:44 -0400

    Move SHA before plugin registration

--- scripts/vault-init.sh
+++ scripts/vault-init.sh
@@ -3,13 +3,12 @@
 kubectl exec -n vault vault-0 -- vault operator init > secrets/vault-init.txt
 kubectl exec -n vault vault-0 -- vault status
 
-read "Copy the root token from secrets/vault-init.txt and put it in secrets.env as `export VAULT_TOKEN=<token>`"
-
-SHA256=$(kubectl exec -n vault vault-0 -- sha256sum /vault/plugins/vault-plugin-secrets-oauth-token-exchange | cut -d ' ' -f1)      
+read "Copy the root token from secrets/vault-init.txt and put it in secrets.env as `export VAULT_TOKEN=<token>`"   
 
 source secrets.env
 
 vault audit enable file file_path=stdout
 
+SHA256=$(kubectl exec -n vault vault-0 -- sha256sum /vault/plugins/vault-plugin-secrets-oauth-token-exchange | cut -d ' ' -f1)   
 vault plugin register -sha256=$SHA256 secret vault-plugin-secrets-oauth-token-exchange
 vault plugin info secret vault-plugin-secrets-oauth-token-exchange
