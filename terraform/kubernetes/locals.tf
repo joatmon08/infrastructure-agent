a57acc7f9b2d484ab7d9738f9a3b2d497b46ca0a
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-04-21 10:44:07 -0400

    Update service name to helloworld-server

--- terraform/kubernetes/locals.tf
+++ terraform/kubernetes/locals.tf
@@ -1,5 +1,5 @@
 locals {
   client_username = "test-client"
-  server_username = "helloworld-agent-server"
+  server_username = "helloworld-server"
   vault_endpoint  = "https://${data.kubernetes_service_v1.vault.status.0.load_balancer.0.ingress.0.hostname}"
 }
