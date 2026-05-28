9a36ef4a48c55eca9cb39da64cf1fe3bef3a20d8
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-05-28 14:09:19 -0400

    Upgrade module to 2.0.0 for eks

--- terraform/base/eks.tf
+++ terraform/base/eks.tf
@@ -2,7 +2,7 @@
 # This module creates both VPC and EKS cluster resources
 module "kubernetes" {
   source  = "app.terraform.io/rosemary-production/kubernetes/aws"
-  version = "1.0.0"
+  version = "2.0.0"
 
   region       = var.aws_region
   cluster_name = var.project_name
