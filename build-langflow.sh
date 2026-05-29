4b858fb01eae18babe0ad4c9285e6a2cb44a172e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-05-29 13:17:07 -0400

    Update build script for Langflow container image

--- build-langflow.sh
+++ build-langflow.sh
@@ -5,5 +5,6 @@ ECR_URI=$(cd terraform/base && terraform output -raw ecr_repository_uri)
 cd Dockerfiles
 
 aws ecr get-login-password --region us-east-1 | podman login --username AWS --password-stdin ${ECR_URI}
-podman build --platform linux/amd64 --file Dockerfile.langflow --tag ${ECR_URI}/infra-agent-langflow:$(git rev-parse HEAD) .
-podman push ${ECR_URI}/infra-agent-langflow:$(git rev-parse HEAD)
+podman build --no-cache --platform linux/amd64 -f Dockerfile.langflow -t infra-agent-langflow:latest .
+podman tag infra-agent-langflow:latest ${ECR_URI}/infra-agent-langflow:$(git rev-parse HEAD)
+podman push --format v2s2 ${ECR_URI}/infra-agent-langflow:$(git rev-parse HEAD)
