aea1ed03ddd668b3ed3eec02d6e79ede27abc210
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-05-29 15:31:07 -0400

    Downgrade langflow due to lfx bug

--- helm/langflow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-helm repo add langflow https://langflow-ai.github.io/langflow-helm-charts
-helm update
-
-helm install langflow langflow/langflow-ide -f langflow.yaml -f langflow-secret.yaml
