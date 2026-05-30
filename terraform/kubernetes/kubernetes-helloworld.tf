56a55eeaa129b82e5657aeb7040b9843889002d0
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-05-29 20:25:04 -0400

    Fix ALB tags for helloworld

--- terraform/kubernetes/kubernetes-helloworld.tf
+++ terraform/kubernetes/kubernetes-helloworld.tf
@@ -7,7 +7,7 @@ resource "kubernetes_ingress_v1" "helloworld_agent_server" {
       "alb.ingress.kubernetes.io/healthcheck-path" = "/.well-known/agent-card.json"
       "alb.ingress.kubernetes.io/inbound-cidrs"    = "${join(",", [for s in var.inbound_cidrs_for_lbs : s])}"
       "alb.ingress.kubernetes.io/success-codes"    = "200,201,404"
-      "alb.ingress.kubernetes.io/tags"             = "Environment=prod,Project=infrastructure-agent,ManagedBy=Terraform"
+      "alb.ingress.kubernetes.io/tags"             = "Environment=${var.environment},Project=${var.project_name},ManagedBy=Terraform"
     }
   }
 
@@ -52,7 +52,7 @@ resource "kubernetes_ingress_v1" "test_client" {
       "alb.ingress.kubernetes.io/healthcheck-path" = "/"
       "alb.ingress.kubernetes.io/inbound-cidrs"    = "${join(",", [for s in var.inbound_cidrs_for_lbs : s])}"
       "alb.ingress.kubernetes.io/success-codes"    = "200"
-      "alb.ingress.kubernetes.io/tags"             = "Environment=prod,Project=infrastructure-agent,ManagedBy=Terraform"
+      "alb.ingress.kubernetes.io/tags"             = "Environment=${var.environment},Project=${var.project_name},ManagedBy=Terraform"
     }
   }
 
