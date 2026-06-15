3ec6defd43100e0f4c73b76acf7eb602c814710d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-06-15 10:50:22 -0400

    Add imports for existing CloudWatch Log Groups

--- terraform/base/imports.tf
+++ terraform/base/imports.tf
@@ -1,4 +1,9 @@
-# import {
-#   to = module.kubernetes.aws_cloudwatch_log_group.eks_cluster
-#   id = "/aws/eks/infra-agent/cluster"
-# }
+import {
+  to = module.kubernetes.aws_cloudwatch_log_group.vpc_flow_logs[0]
+  id = "/aws/vpc/infra-agent"
+}
+
+import {
+  to = module.kubernetes.module.eks.aws_cloudwatch_log_group.this[0]
+  id = "/aws/eks/infra-agent/cluster"
+}
