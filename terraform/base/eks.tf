4af8756b9930554e76bc92a4790099a4ff43bdc2
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-07-22 14:56:09 -0400

    Upgrade kubernetes module to 4.0.0 and remove imports.tf
    Approved with IBM Bob

--- terraform/base/eks.tf
+++ terraform/base/eks.tf
@@ -2,20 +2,11 @@
 # This module creates both VPC and EKS cluster resources
 module "kubernetes" {
   source  = "app.terraform.io/rosemary-production/kubernetes/aws"
-  version = "3.0.1"
+  version = "4.0.0"
 
   region                               = var.aws_region
   cluster_name                         = var.project_name
   node_group_desired_size              = 3
   node_group_instance_types            = ["t3.xlarge"]
-  enable_guardduty_eks_protection      = false
   cluster_endpoint_public_access_cidrs = var.inbound_cidrs_for_lbs
 }
-
-# Note: GuardDuty EKS protection is disabled (enable_guardduty_eks_protection = false)
-# because it's managed by another team via Service Control Policy
-
-# import {
-#   id = "kube-system/aws-load-balancer-controller"
-#   to = module.kubernetes.helm_release.aws_load_balancer_controller
-# }
