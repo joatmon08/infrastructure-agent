72208ff36d1391b77f4bac386f5ff963d3fc2899
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-07-22 10:03:43 -0400

    Remove stale import blocks for non-existent CloudWatch log groups
    Approved with IBM Bob

--- terraform/base/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = module.kubernetes.aws_cloudwatch_log_group.eks_cluster
-  id = "/aws/eks/infra-agent/cluster"
-}
-
-import {
-  to = module.kubernetes.aws_cloudwatch_log_group.vpc_flow_logs[0]
-  id = "/aws/vpc/infra-agent"
-}
