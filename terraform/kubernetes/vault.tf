c7fba6526327ed8de6c53b4b39f580042023c9e3
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2026-08-13 16:08:22 -0400

    Add inbound-cidr annotations that won't be overriden

--- terraform/kubernetes/vault.tf
+++ terraform/kubernetes/vault.tf
@@ -36,7 +36,7 @@ resource "kubernetes_ingress_v1" "vault_ui" {
       "alb.ingress.kubernetes.io/scheme"           = "internet-facing"
       "alb.ingress.kubernetes.io/target-type"      = "ip"
       "alb.ingress.kubernetes.io/healthcheck-path" = "/v1/sys/health"
-      "alb.ingress.kubernetes.io/inbound-cidrs"    = var.allow_hcp_terraform_to_access_vault ? "0.0.0.0/0" : "${join(",", concat(var.inbound_cidrs_for_lbs, [data.terraform_remote_state.base.outputs.vpc_cidr_block]))}"
+      "alb.ingress.kubernetes.io/inbound-cidrs"    = var.allow_hcp_terraform_to_access_vault ? "0.0.0.0/0" : join(",", [for s in var.inbound_cidrs_for_lbs : s], data.terraform_remote_state.base.outputs.vpc_cidr_block)
       "alb.ingress.kubernetes.io/success-codes"    = "200,204"
       "alb.ingress.kubernetes.io/tags"             = "Environment=${var.environment},Project=${var.project_name},ManagedBy=Terraform"
       "alb.ingress.kubernetes.io/backend-protocol" = "HTTPS"
